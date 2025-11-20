08325536e6dad9f984f0589835779073d2c382d4
sayyani <ameensayani204@gmail.com>
2025-11-20 12:46:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Voyagr from "./components/Voyagr";
+
+export default function App() {
+  return <Voyagr />;
+}
---
+++ src/components/Voyagr.jsx
@@ -0,0 +1,3 @@
+export default function Voyagr() {
+  return <div>Voyagr UI goes here...</div>;
+}
